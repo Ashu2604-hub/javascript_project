f10b44de5f4bf97bec4b500da9da47f17e6fc380
Ashu2604-hub <111137247+Ashu2604-hub@users.noreply.github.com>
2023-10-11 17:49:22 +0000

    conatainer

---
+++ pro/variables.js
@@ -0,0 +1,14 @@
+const accountId = 123
+let accountEmail = "ashu@gmail.com"
+var accountPassword = "2345"
+
+accountCity = "Pune"
+accountEmail="ole@gmil.com"
+accountPasswor="212211"
+
+//accountId = 2
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
